8e2a171ec486beea4d06ab594ef8c2f1ccf91b9e
Arjun <malli.arjun.kp@gmail.com>
2020-03-11 15:42:11 +0530

    Working create_book route

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-const ejs = require('ejs');
+
+const Books = require('./controller/books');
 
 //  DB connection
 mongoose.connect('mongodb://localhost:27017/testdb', { useNewUrlParser: 'true' });
@@ -26,14 +27,7 @@ app.use(express.static('views'));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.render('create_book.ejs');
-});
-app.use(expressLayouts);
-const async = require('async');
-const Books = require('./controller/books');
-
-app.use('/books', Books);
+app.use('/', Books);
 
 app.listen(3000, () => {
   console.log(`server started at port${3000}`);
--- controller/books.js
+++ controller/books.js
@@ -1,44 +1,37 @@
 const express = require('express');
+
 const router = express.Router();
-const Books = require("../Model/books_model")
+const Books = require('../Model/books_model');
 
 router.get('/', async (req, res) => {
-  console.log("hi")
-  var books = await BooksModel.find({});
-  res.render("create_book")
+  console.log('hi');
+  const books = await BooksModel.find({});
+  res.render('create_book');
 });
 
 router.post('/create', async (req, res) => {
   const { bookName, cost, auth } = req.body;
-  var NewProduct = new Books({ bookName, cost, auth });
+  const NewProduct = new Books({ bookName, cost, auth });
   try {
     const book = await Books.create(NewProduct);
-    if(book){
+    if (book) {
       // req.flash("message", " Equipment Characteristic have been saved successfully");
-			res.render("books/create_book", { book:book });
-  }
+      res.render('books/create_book', { book });
+    }
   } catch (error) {
     res.json({
       success: false,
-      message: error
+      message: error,
     });
   }
 });
 
 router.get('/create_book', (req, res) => {
-  res.render("create_book.ejs");
-
-
+  res.render('create_book.ejs');
 });
 
+router.post('/update', (req, res) => {});
 
-router.post('/update', (req, res) => {
-
-});
-
-router.delete("/delete", (req, res) => {
-
-});
-
+router.delete('/delete', (req, res) => {});
 
 module.exports = router;
